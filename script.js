b7c7c41a85cd42a9ae88f01240bb90d8f1672066
Oxamyt <dentez.danil@gmail.com>
2023-11-26 20:11:49 +0200

    add-password-validation

---
+++ script.js
@@ -0,0 +1,15 @@
+let password = document.getElementById('password');
+let confirm = document.getElementById('confirm-password')      
+
+let validate = function () {
+if(password.value != confirm.value){   
+confirm.classList.add('invalid'); 
+}
+else{
+    confirm.classList.remove('invalid');
+    confirm.classList.add('valid');
+}
+}
+
+password.onchange = validate;
+confirm.onkeyup = validate;
